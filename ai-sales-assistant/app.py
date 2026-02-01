f070742d2937975185ee24dfb16b0ca1e859ba54
Slindile <slindilenkabs23@gmail.com>
2026-02-01 21:48:17 +0200

    first web application using streamlit

---
+++ ai-sales-assistant/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+# from utils import generate_ai_insight
+
+st.title("AI Sales Insights Assistant")
+st.write("Upload your CSV file to get started")
